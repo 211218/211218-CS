829b16c82110f0f581a1b25d9c7e661102adaa51
211125 <211125@ids.upchiapas.edu.mx>
2022-07-30 04:41:48 -0500

    reponsive table products c/u

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import '../StyleSheets/Dashboard.css';
-import img1 from '../Img/js.png';
+import img1 from '../Img/elixir.png';
 const Dashboard = () =>{
     return(
         <div>
@@ -13,7 +13,7 @@ const Dashboard = () =>{
                             <img className='img-all' src={img1}></img>
                         </div>
                         <div className='product-title'>
-                            <h1 className='title'>Cereal</h1>
+                            <h1 className='title'>Elixir</h1>
                         </div>
                         <div className='product-price'>
                             <h1 className='title-number'>$23</h1>
@@ -26,7 +26,7 @@ const Dashboard = () =>{
                         <div className='product-stock'>
                             <h1 className='title-number'>Stock: 12</h1>
                         </div>
-                    </div>
+                    </div>    
                 </div>
             </div>
         </div>
